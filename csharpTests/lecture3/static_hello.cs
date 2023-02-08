818adc2096c50e6d3887d03fe19039182d9091f9
frederikefj <frederikefj@gmail.com>
2023-02-08 09:24:00 +0100

    added lecture 3 code

---
+++ csharpTests/lecture3/static_hello.cs
@@ -0,0 +1,5 @@
+public static class static_hello{
+	public static string greeting = "hello\n";
+	public static void print(){ System.Console.Write(greeting); }
+}
+
---
+++ csharpTests/plot.cs
@@ -0,0 +1,5 @@
+class plot{
+  static void Main(){
+	System.Console.Write("12345678901234567890\n");
+  }
+}
