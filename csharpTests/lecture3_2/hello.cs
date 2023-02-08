297673cd36d08b3dc555db1c6b22f2728529ca2d
frederikefj <frederikefj@gmail.com>
2023-02-08 10:08:59 +0100

    added second part of lecture3

---
+++ csharpTests/lecture3_2/hello.cs
@@ -0,0 +1,6 @@
+public class hello{
+	public string greeting="defealt greeting";
+	public void print(){System.Console.Write(greeting);}
+	public hello(string init){greeting = init;}
+
+}
--- csharpTests/lecture3_2/main.cs
+++ csharpTests/lecture3_2/main.cs
@@ -1,7 +1,8 @@
 using System;
 using static System.Console;
 using static System.Math;
-class main{
+
+public static class main{
 	public static void Main(){
 	Write("hello from main\n");
 	static_hello.print();
@@ -10,5 +11,20 @@ class main{
 	static_world.greeting="new world from main\n";
 	static_hello.print();
 	static_world.print();
+	hello hello1 = new hello("hello1\n");
+	hello world1 = new hello("world1\n");
+	hello1.print();
+	world1.print();
+	hello another_hello = hello1;
+	another_hello.greeting = "another greeting\n";
+	hello1.print();
+	Write($"the value of pi in Math is {PI}\n");
+	double sqrt2 = Sqrt(2.0);
+	Write($"sqrt2^2 = {sqrt2*sqrt2}\n");
+	Write($"1/2 = {1/2}\n");
+	Write($"1.0/2 = {1.0/2}\n");
+	
 	}
 }
+
+
